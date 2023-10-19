e22cdff0bcade978cab41773fe99b8aa6d82f287
Daniel <danieljon2302@gmail.com>
2023-10-20 07:11:33 +0800

    firstCommit

---
+++ src/main/java/com/daniel/shoppingPlatform/ShoppingPlatformProjectApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.shoppingPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingPlatformProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingPlatformProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/shoppingPlatform/ShoppingPlatformProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.shoppingPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingPlatformProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
